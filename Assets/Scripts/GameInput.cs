5abb06642e248e0fceb435e39de89999a4bd73f8
macovei-alexandru <maccelmare@gmail.com>
2024-11-20 20:50:05 +0200

    added new system input actions

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+	public static GameInput Instance { get; private set; }
+
+	private InputSystem_Actions inputActions;
+
+	private void Awake()
+	{
+		if (Instance != null)
+		{
+			Debug.LogError("Multiple instances of GameInput found!");
+			return;
+		}
+		Instance = this;
+
+		inputActions = new InputSystem_Actions();
+		inputActions.Player.Enable();
+	}
+
+	public Vector2 GetInputVectorNormalized()
+	{
+		return inputActions.Player.Move.ReadValue<Vector2>();
+	}
+}
