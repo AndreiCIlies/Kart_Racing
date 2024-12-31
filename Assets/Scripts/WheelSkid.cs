6d182be021a990db45315f8ca77917a6713d0c0e
macovei-alexandru <maccelmare@gmail.com>
2024-12-31 14:43:29 +0200

    added skid marks for back wheels

---
+++ Assets/Scripts/WheelSkid.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class WheelSkid : MonoBehaviour
+{
+	[SerializeField] private WheelCollider wheelCollider;
+	[SerializeField] private float slipThreshold = 0.3f;
+
+	private TrailRenderer trailRenderer;
+
+	public void Start()
+	{
+		trailRenderer = GetComponent<TrailRenderer>();
+
+		Assert.IsNotNull(trailRenderer, "No component of type TrailRenderer found for WheelSkid.");
+		Assert.IsNotNull(wheelCollider, "Wheel Collider is not assigned.");
+
+		trailRenderer.emitting = false;
+	}
+
+	public void Update()
+	{
+		trailRenderer.emitting = IsWheelSliding();
+	}
+
+	private bool IsWheelSliding()
+	{
+		if (wheelCollider.GetGroundHit(out WheelHit wheelHit))
+		{
+			if (Mathf.Abs(wheelHit.forwardSlip) > slipThreshold)
+			{
+				return true;
+			}
+			if (Mathf.Abs(wheelHit.sidewaysSlip) > slipThreshold)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
