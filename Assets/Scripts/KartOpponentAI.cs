64b5f3cd6521c1d0e9bbe93cb498bbba59afd1fd
Durbaca <dianadurbaca2003@gmail.com>
2025-01-02 17:27:57 +0200

    Added finish line, waypoint system, and adjusted materials

--- Assets/Scripts/KartOpponentAI.cs
+++ Assets/Scripts/KartOpponentAI.cs
@@ -1,22 +1,54 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class KartOpponentAI : MonoBehaviour
 {
+    public Transform[] waypoints;
     public Transform finishLine; 
     private NavMeshAgent agent;
+    private int currentWaypointIndex = 0;
+    private bool isHeadingToFinish = false; 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(finishLine.position); 
+
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+        else
+        {
+            Debug.LogWarning("Nu exista waypoint-uri setate!");
+        }
     }
 
     void Update()
     {
         if (!agent.pathPending && agent.remainingDistance < 1f)
         {
-            agent.isStopped = true; 
+            if (!isHeadingToFinish)
+            {
+                currentWaypointIndex++;
+
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    isHeadingToFinish = true;
+                    agent.SetDestination(finishLine.position); 
+                }
+                else
+                {
+                    agent.SetDestination(waypoints[currentWaypointIndex].position);
+                }
+            }
+            else
+            {
+                if (agent.remainingDistance < 1f)
+                {
+                    agent.isStopped = true;
+                    Debug.Log("Kartul a ajuns la linia de finish!");
+                }
+            }
         }
     }
 }
