c2ea5e62bc1d98898ad8ae06b5c4c2ef7cef1ab2
macovei-alexandru <maccelmare@gmail.com>
2025-01-08 18:19:00 +0200

    added look bac functionality

--- Assets/Scripts/KartFollow.cs
+++ Assets/Scripts/KartFollow.cs
@@ -11,9 +11,9 @@ public class KartFollow : MonoBehaviour
 
 	[SerializeField] private Transform kartTransform;
 	[SerializeField] private Vector3 firstOffset = new(0, 1, 0);
-	[SerializeField] private Vector3 firstLookAtPosition = new(0, 0, 4);
+	[SerializeField] private Vector3 firstLookAtOffset = new(0, 0, 4);
 	[SerializeField] private Vector3 thirdOffset = new(0, 2, -4);
-	[SerializeField] private Vector3 thirdLookAtPoisiton = new(0, 0, 2);
+	[SerializeField] private Vector3 thirdLookAtOffset = new(0, 0, 2);
 	[SerializeField] private float thirdSmoothFactor = 0.8f;
 
 	private CameraMode cameraMode;
@@ -27,6 +27,9 @@ public class KartFollow : MonoBehaviour
 
 	public void FixedUpdate()
 	{
+		bool isLookingAhead = !Input.GetKey(KeyCode.Space);
+		Vector3 direction = new Vector3(1, 1, isLookingAhead ? 1 : -1);
+
 		if (Input.GetKey(KeyCode.Alpha1))
 		{
 			cameraMode = CameraMode.FirstPerson;
@@ -38,15 +41,15 @@ public class KartFollow : MonoBehaviour
 
 		if (cameraMode == CameraMode.FirstPerson)
 		{
-			Vector3 desiredPosition = kartTransform.TransformPoint(firstOffset);
+			Vector3 desiredPosition = kartTransform.TransformPoint(Vector3.Scale(firstOffset, direction));
 			transform.position = desiredPosition;
-			transform.LookAt(kartTransform.TransformPoint(firstLookAtPosition));
+			transform.LookAt(kartTransform.TransformPoint(Vector3.Scale(firstLookAtOffset, direction)));
 		}
 		else if (cameraMode == CameraMode.ThirdPerson)
 		{
-			Vector3 desiredPosition = kartTransform.TransformPoint(thirdOffset);
+			Vector3 desiredPosition = kartTransform.TransformPoint(Vector3.Scale(thirdOffset, direction));
 			transform.position = Vector3.Lerp(transform.position, desiredPosition, thirdSmoothFactor);
-			transform.LookAt(kartTransform.TransformPoint(thirdLookAtPoisiton));
+			transform.LookAt(kartTransform.TransformPoint(Vector3.Scale(thirdLookAtOffset, direction)));
 		}
 	}
 }
