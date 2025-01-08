454c544cf22671a28c33abbc0fd852258b443506
macovei-alexandru <maccelmare@gmail.com>
2025-01-08 18:53:59 +0200

    small changes

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private GameObject controlsPanel;
-    [SerializeField] private GameObject authorsPanel;
+	[SerializeField] private GameObject mainMenuPanel;
+	[SerializeField] private GameObject controlsPanel;
+	[SerializeField] private GameObject authorsPanel;
 
-    public void ShowControlsPanel()
-    {
-        mainMenuPanel.SetActive(false);
-        controlsPanel.SetActive(true);
-        authorsPanel.SetActive(false);
-    }
+	public void ShowControlsPanel()
+	{
+		mainMenuPanel.SetActive(false);
+		controlsPanel.SetActive(true);
+		authorsPanel.SetActive(false);
+	}
 
-    public void ShowMainMenuPanel()
-    {
-        controlsPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);
-        authorsPanel.SetActive(false);
-    }
+	public void ShowMainMenuPanel()
+	{
+		controlsPanel.SetActive(false);
+		mainMenuPanel.SetActive(true);
+		authorsPanel.SetActive(false);
+	}
 
-    public void ShowAuthorsPanel()
-    {
-        mainMenuPanel.SetActive(false);
-        controlsPanel.SetActive(false);
-        authorsPanel.SetActive(true);
-    }
+	public void ShowAuthorsPanel()
+	{
+		mainMenuPanel.SetActive(false);
+		controlsPanel.SetActive(false);
+		authorsPanel.SetActive(true);
+	}
 
-    public void StartGame()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("s1");
-    }
+	public void StartGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("RaceScene");
+	}
 }
